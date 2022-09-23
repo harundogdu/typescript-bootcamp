df280a89b91921e0ccb341e16f84b6082dbf2503
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-23 16:31:38 +0300

    feat(typescript) : Type definitions added

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Type definitions
+let number: number = 1;
+let number2 = 1 as number;
+
+let string: string = "string";
+let string2 = "string" as string;
+
+let boolean: boolean = true;
+let boolean2 = true as boolean;
+
+let array: number[] = [1, 2, 3];
+let array2: Array<number> = [1, 2, 3];
+let array3 = [1, 2, 3] as number[];
+let array4 = [1, 2, 3] as Array<number>;
+
+let object: object = { a: "example" };
+let object2: { a: number } = { a: 1 };
+let object3 = { a: 1 } as { a: number };
+
+let tuple: [number, string] = [1, "string"];
+let tuple2 = [1, "string"] as [number, string];
+
+let any: any = 1;
+let any2 = 1 as any;
