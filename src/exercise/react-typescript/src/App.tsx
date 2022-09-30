ff08176c8325c2bd70123d771095d865bf0b0305
Harun Doğdu <Harundogdu06@gmail.com>
2022-09-30 18:57:11 +0300

    feat(typescript) : react components used

--- src/exercise/react-typescript/src/App.tsx
+++ src/exercise/react-typescript/src/App.tsx
@@ -1,11 +1,17 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
-import { CountryItem, Loading } from './components';
+import { Button, CountryItem, Input, Loading, Status } from './components';
 import { ICountry } from './types';
 
 function App() {
   const [countries, setCountries] = React.useState<ICountry[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
+
+  const [search, setSearch] = React.useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
 
   const fetchCountries = async () => {
     try {
@@ -26,11 +32,26 @@ function App() {
 
   return (
     <div className='App'>
+      {/**
+       * Loading component
+       */}
       <Loading loading={loading}>
         {countries.map(country => (
           <CountryItem key={country.name.common} country={country} />
         ))}
       </Loading>
+      {/**
+       * Input component
+       */}
+      <Input value={search} onChange={handleChange} />
+      {/**
+       * Status component
+       */}
+      <Status status='info' />
+      {/**
+       * Button component
+       */}
+      <Button title='Click Me!' onClick={() => console.log('Clicked!')} />
     </div>
   );
 }
--- src/exercise/react-typescript/src/components/index.ts
+++ src/exercise/react-typescript/src/components/index.ts
@@ -1,2 +1,5 @@
 export { default as Loading } from './Loading';
 export { default as CountryItem } from './CountryItem';
+export { default as Input } from './Input';
+export { default as Status } from './Status';
+export { default as Button } from './Button';
